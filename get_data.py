8a2e04505e95673d81a5522c56b40ad325d3c1b8
Miguel Ozana <miguelozana74@gmail.com>
2026-02-03 02:26:25 -0300

    get_data.py script added.

---
+++ get_data.py
@@ -0,0 +1,29 @@
+import requests as req
+import pandas as pd
+
+def get_data(cep):
+    endpoint = f"https://viacep.com.br/ws/{cep}/json/"
+
+    res = req.get(endpoint)
+
+    if res.status_code == 200:
+      return res.json()
+    else:
+      return {"error": "CEP not found"}
+
+users_path = "bronze/users.csv"
+users_df = pd.read_csv(users_path)
+
+cep_list = users_df["cep"].tolist()
+cep_info_list = []
+
+for cep in cep_list:
+  cep_info = get_data(cep.replace("-", ""))
+  cep_info_list.append(cep_info)
+  if "error" in cep_info:
+    continue
+  print(cep_info)
+
+cep_info_df = pd.DataFrame(cep_info_list)
+cep_info_df.to_csv("bronze/cep_info.csv", index=False)
+
