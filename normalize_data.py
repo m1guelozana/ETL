599fffbb948aceb20d92396584c23187e907cbd7
Miguel Ozana <miguelozana74@gmail.com>
2026-02-03 20:25:30 -0300

    normalize_data.py is a class now

--- normalize_data.py
+++ normalize_data.py
@@ -1,39 +1,43 @@
 import os
 import pandas as pd
 
-# 1 diretório de entrada e 1 diretório de saída.
-# diretório de entrada: bronze
-# diretório de saída: silver
 
-input_path = "bronze" # diretório de entrada, onde estão os dados brutos. Ler arquivos da pasta bronze (csv e json)
-output_path = "silver" # diretório de saída, onde estarão os dados normalizados. Salvar com parquet
+class NormalizeData:
+    def __init__(self, input_path, output_path):
+        self.input_path = input_path
+        self.output_path = output_path
+        os.makedirs(self.output_path, exist_ok=True)
 
-os.makedirs(output_path, exist_ok=True)
+    # Normalização de dados
+    def normalize_data(self):
+        # Iterar sobre os arquivos no diretório de entrada
+        for file in os.listdir(self.input_path):
+            file_path = os.path.join(self.input_path, file)
 
-# Iterar sobre os arquivos no diretório de entrada
-for file in os.listdir(input_path):
-    file_path = os.path.join(input_path, file)
+            if file.endswith(".csv"):
+                df = pd.read_csv(file_path)
+            elif file.endswith(".json"):
+                df = pd.read_json(file_path)
+            else:
+                print(f"Unsupported file format: {file}")
+                continue
 
-    if file.endswith(".csv"):
-        df = pd.read_csv(file_path)
-        output_file = os.path.join(output_path, file.replace(".csv", ".parquet"))
-        df.to_parquet(output_file, index=False)
-    elif file.endswith(".json"):
-        df = pd.read_json(file_path)
-        output_file = os.path.join(output_path, file.replace(".json", ".parquet"))
-        df.to_parquet(output_file, index=False)
-    else:
-        print(f"Unsupported file format: {file}")
+            # Converter colunas que são listas convertendo-as em strings
+            for col in df.columns:
+                if df[col].apply(lambda x: isinstance(x, list)).any():
+                    df[col] = df[col].apply(lambda x: str(x) if isinstance(x, list) else x)
 
-    # Converter colunas que são listas convertendo-as em strings
-    for col in df.columns:
-        if df[col].apply(lambda x: isinstance(x, list)).any():
-            df[col] = df[col].apply(lambda x: str(x) if isinstance(x, list) else x)
+            df = df.drop_duplicates().reset_index(drop=True)
+
+            # Salvar como parquet
+            output_file = os.path.join(self.output_path, file.rsplit('.', 1)[0] + ".parquet")
+            df.to_parquet(output_file, index=False)
+            print(f"Processed and saved: {output_file}")
 
 
-    df = df.drop_duplicates().reset_index(drop=True)
+if __name__ == "__main__":
+    input_path = "bronze"
+    output_path = "silver"
 
-    # Salvar como parquet
-    df.to_parquet(output_file, index=False)
-    print(f"Processed and saved: {output_file}")
-
+    normalizer = NormalizeData(input_path, output_path)
+    normalizer.normalize_data()
