7993c7e3d8bc26c6ca3206aa01bdc4b97d342c02
Miguel Ozana <miguelozana74@gmail.com>
2026-02-03 20:37:56 -0300

    normalize_data.py is a class now

--- normalize_data.py
+++ normalize_data.py
@@ -8,29 +8,36 @@ class NormalizeData:
         self.output_path = output_path
         os.makedirs(self.output_path, exist_ok=True)
 
+    def convert_to_string(self, df):
+        # Converter colunas que são listas convertendo-as em strings
+        for col in df.columns:
+            if df[col].apply(lambda x: isinstance(x, list)).any():
+                df[col] = df[col].apply(lambda x: str(x) if isinstance(x, list) else x)
+        return df
+
+    def load_df_from_file(self, file):
+        input_path = os.path.join(self.input_path, file)
+        _, ext = os.path.splitext(file)
+
+        # Carregar DataFrame a partir de arquivo CSV ou JSON
+        if ext.lower() == ".csv":
+            df = pd.read_csv(input_path)
+        elif ext.lower() == ".json":
+            df = pd.read_json(input_path)
+        else:
+            raise ValueError(f"Unsupported file format: {ext}")
+        return df
+
     # Normalização de dados
     def normalize_data(self):
-        # Iterar sobre os arquivos no diretório de entrada
         for file in os.listdir(self.input_path):
-            file_path = os.path.join(self.input_path, file)
+            df = self.load_df_from_file(file)
+            output_file = os.path.join(self.output_path, file)
 
-            if file.endswith(".csv"):
-                df = pd.read_csv(file_path)
-            elif file.endswith(".json"):
-                df = pd.read_json(file_path)
-            else:
-                print(f"Unsupported file format: {file}")
-                continue
-
-            # Converter colunas que são listas convertendo-as em strings
-            for col in df.columns:
-                if df[col].apply(lambda x: isinstance(x, list)).any():
-                    df[col] = df[col].apply(lambda x: str(x) if isinstance(x, list) else x)
-
+            df = self.convert_to_string(df)
             df = df.drop_duplicates().reset_index(drop=True)
 
             # Salvar como parquet
-            output_file = os.path.join(self.output_path, file.rsplit('.', 1)[0] + ".parquet")
             df.to_parquet(output_file, index=False)
             print(f"Processed and saved: {output_file}")
 
